29b99bd66b57c0c03c54b5bc1e5c0b6c233a8b8f
root <joelcjohnson123@gmail.com>
2020-08-05 21:25:23 +0530

    solved the reloading issue while clicking on homebutton

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const styles = theme => ({
         zIndex: 9
     },
     link: {
-        margin: theme.spacing(),
+        margin: '0',
         textDecoration: 'none',
         color: 'white'
     },
@@ -25,37 +25,26 @@ const styles = theme => ({
 });
 
 function Header({ classes }) {
+    const currUrl = window.location.href
     return (
-        
-        <Fragment>
-            {/* <Grid item xs={12} style={{position: 'fixed', width: '100%'}} className={classes.root}> */}
-            <Grid container style={{position: 'fixed', width: '100%'}} className={classes.root} >
-            <Grid item xs={3} style={{paddingLeft: '10px'}}>
-                <Link to="/" className={classes.link}>
-                    <Typography variant="h5" className={classes.grow}>
-                        AutographaMT
-                    </Typography>
-                </Link>
-            </Grid>
-            <Grid item xs={9} style={{textAlign: 'right'}}>
+        <Grid container style={{position: 'fixed', width: '100%', backgroundColor: 'black' }}  >
+           { 
+           currUrl.split('/').pop() =='' ? (
+               <Grid item xs={3} style={{padding: '10px'}}>
+                      <span style={{fontSize: '25px', color:"white"}}>AutographaMT</span>  
+                </Grid>
+                ):(
+                <Grid item xs={3} style={{padding: '10px'}}>
+                    <Link to="/" className={classes.link}>
+                          <span style={{fontSize: '25px'}}>AutographaMT</span>  
+                    </Link>
+                </Grid>)
+            }
+            <Grid item xs={9} style={{padding:'10px', textAlign: 'right'}}>
                 <SignedInLinks classes={classes} />
             </Grid>
+
         </Grid>
-        </Fragment>
-        // <AppBar className={classes.h1}>
-        //     <Toolbar>
-        //         {/* <Link color="inherit" variant="body2" href="/"> */}
-        //         <Link to="/" className={classes.link}>
-        //             <Typography variant="h5" className={classes.grow}>
-        //                 AutographaMT
-        //             </Typography>
-        //         </Link>
-        //     <div className={classes.grow}>
-        //         </div>
-        //         <SignedInLinks classes={classes} />
-        //     </Toolbar>
-        // </AppBar>
     )
 }
-
 export default withStyles(styles)(Header);
