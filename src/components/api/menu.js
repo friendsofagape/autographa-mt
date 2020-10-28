3281530f4bea29ba3df301bc22398400e091d53a
root <joelcjohnson123@gmail.com>
2020-10-28 19:09:41 +0530

    modified the dash board menu names

--- src/components/api/menu.js
+++ src/components/api/menu.js
@@ -1,47 +1,46 @@
 export const menus = [
     {
-        name: 'Dashboard',
-        roles: ['m', 'ad', 'sa'],
-        key:'dashboard',
-        child: [
-            {
-                name: 'List organisation',
-                link: '/app/organisations',
-                roles: ['member', 'admin', 'sa'],
-                key:'organisations'
-            },
-            {
-                name: 'List projects',
-                link: '/app/projects',
-                roles: ['member', 'ad', 'sa'],
-                key: 'projects'
-            },
-            {
-                name: 'List users',
-                link: '/app/users',
-                roles: ['member', 'admin', 'sa'],
-                key: 'users'
-            },
-            {
-                name: 'Create Organisation',
-                link: '/app/organisations/create',
-                roles: ['m', 'sa'],
-                key: 'create_organisations'
-            },
-            {
-                name: 'Source Languages',
-                link: '/app/translations/sources',
-                roles: ['m', 'ad', 'sa'],
-                key: 'sources'
-            },
-            {
-                name: 'My projects',
-                link: '/app/translations/projects',
-                roles: ['m', 'ad', 'sa'],
-                key: 'my_projects'
-            }
-
-        ]
+      name: "Dashboard",
+      roles: ["m", "ad", "sa"],
+      key: "dashboard",
+      child: [
+        {
+          name: "My Projects",
+          link: "/app/translations/projects",
+          roles: ["m", "ad", "sa"],
+          key: "my_projects",
+        },
+        {
+          name: "Available Sources",
+          link: "/app/translations/sources",
+          roles: ["m", "ad", "sa"],
+          key: "sources",
+        },
+        {
+          name: "Create Organisation",
+          link: "/app/organisations/create",
+          roles: ["m", "sa"],
+          key: "create_organisations",
+        },
+        {
+          name: "Verify Organisation",
+          link: "/app/organisations",
+          roles: ["member", "admin", "sa"],
+          key: "organisations",
+        },
+        {
+          name: "Assign Projects",
+          link: "/app/projects",
+          roles: ["member", "ad", "sa"],
+          key: "projects",
+        },
+        {
+          name: "Verify Users",
+          link: "/app/users",
+          roles: ["member", "admin", "sa"],
+          key: "users",
+        },
+      ],
     },
     // {
     //     name: 'Translations',
@@ -62,5 +61,5 @@ export const menus = [
     //         },
     //     ]
     // },
-
-]
+  ];
+  
