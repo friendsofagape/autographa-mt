d6785c304f1f37e756091757ed5476ee776efabb
Bobby <bobbykumar706584@gmail.com>
2021-08-10 12:22:01 +0530

    fixed create source issue and added common book folder

--- src/components/Translations/MenuBar.js
+++ src/components/Translations/MenuBar.js
@@ -1,94 +1,114 @@
-import React, { Component } from 'react';
-import { FormControl, Grid, MenuItem, Select, InputLabel } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
-import { connect } from 'react-redux';
-import compose from 'recompose/compose';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+	FormControl,
+	Grid,
+	MenuItem,
+	Select,
+	InputLabel,
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import compose from "recompose/compose";
+import { withRouter } from "react-router-dom";
+import { books } from "../Common/BibleOldNewTestment";
 
-const styles = theme => ({
-    selectionGrid: {
-        marginLeft: '4%',
-    },
-    formControl: {
-        minWidth: 120,
-      },
-    selectMenu: {
-        width: '140px',
-        padding: '2px',
-        border: '1px solid black',
-        borderRadius: '10px'
-    },
+const styles = (theme) => ({
+	selectionGrid: {
+		marginLeft: "4%",
+	},
+	formControl: {
+		minWidth: 120,
+	},
+	selectMenu: {
+		width: "140px",
+		padding: "2px",
+		border: "1px solid black",
+		borderRadius: "10px",
+	},
 });
 
 class MenuBar extends Component {
-    state= {
-        selectbook:''
-    }
-    
-    // sort the books in bible order
-    displayBooks() {
-        const { userProjects, location } = this.props
-        const books = ["gen", "exo", "lev", "num", "deu", "jos", "jdg", "rut", "1sa", "2sa", 
-        "1ki", "2ki", "1ch", "2ch", "ezr", "neh", "est", "job", "psa", "pro", "ecc", "sng", 
-        "isa", "jer", "lam", "ezk", "dan", "hos", "jol", "amo", "oba", "jon", "mic", "nam", "hab",
-        "zep", "hag", "zec", "mal", "mat", "mrk", "luk", "jhn", "act", "rom", "1co", "2co", "gal",
-        "eph", "php", "col", "1th", "2th", "1ti", "2ti", "tit", "phm", "heb", "jas", "1pe", "2pe", "1jn", 
-        "2jn", "3jn", "jud", "rev"]
-        if (userProjects.length > 0 ) {
-            const data = userProjects.filter(project => project.projectId === parseInt(location.pathname.split('/').pop()));
-            let assignedBooks = [];
-            books.map((book)=>{                                                                              //map function for pushing the books in order
-            return data[0].books.includes(book)? assignedBooks.push(book): null
-            })
-            return assignedBooks.map(item => {                                                               //map function for displaying books on UI
-                return ( 
-                    <MenuItem key={item} value={item} style={{fontSize:"80%"}}>{item.toUpperCase()}</MenuItem>
-                )
-            })
-        } else {
-            return (
-                <MenuItem key="" value="" disabled>No books assigned</MenuItem>
-            )
-        }
-    }
+	state = {
+		selectbook: "",
+	};
 
-    onSelect = (e) => {
-        this.setState({
-            selectbook:e.target.value
-        })
-        this.props.updateState(e.target.value);
-    }
+	// sort the books in bible order
+	displayBooks() {
+		const { userProjects, location } = this.props;
 
-    render() {
-        const { classes } = this.props
-        return (
-            <FormControl className={classes.formControl}>
-                <InputLabel id="demo-simple-select-label">Select Book</InputLabel>
-                <Select
-                    margin="dense"
-                    value={this.state.selectbook}
-                    onChange={this.onSelect}
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                >
-                {this.displayBooks()}
-                </Select>
-            </FormControl>
-        )
-    }
+		if (userProjects.length > 0) {
+			const data = userProjects.filter(
+				(project) =>
+					project.projectId ===
+					parseInt(location.pathname.split("/").pop())
+			);
+			let assignedBooks = [];
+			books.map((book) => {
+				//map function for pushing the books in order
+				return data[0].books.includes(book)
+					? assignedBooks.push(book)
+					: null;
+			});
+			return assignedBooks.map((item) => {
+				//map function for displaying books on UI
+				return (
+					<MenuItem
+						key={item}
+						value={item}
+						style={{ fontSize: "80%" }}
+					>
+						{item.toUpperCase()}
+					</MenuItem>
+				);
+			});
+		} else {
+			return (
+				<MenuItem key="" value="" disabled>
+					No books assigned
+				</MenuItem>
+			);
+		}
+	}
+
+	onSelect = (e) => {
+		this.setState({
+			selectbook: e.target.value,
+		});
+		this.props.updateState(e.target.value);
+	};
+
+	render() {
+		const { classes } = this.props;
+		return (
+			<FormControl className={classes.formControl}>
+				<InputLabel id="demo-simple-select-label">
+					Select Book
+				</InputLabel>
+				<Select
+					margin="dense"
+					value={this.state.selectbook}
+					onChange={this.onSelect}
+					labelId="demo-simple-select-label"
+					id="demo-simple-select"
+				>
+					{this.displayBooks()}
+				</Select>
+			</FormControl>
+		);
+	}
 }
 
 const mapStateToProps = (state) => {
-    return {
-        userProjects: state.project.userProjects,
-    }
-}
+	return {
+		userProjects: state.project.userProjects,
+	};
+};
 
 const mapDispatchToProps = (dispatch) => ({
-    dispatch
-})
+	dispatch,
+});
 
 export default compose(
-    withStyles(styles),
-    connect(mapStateToProps, mapDispatchToProps)
- )(withRouter(MenuBar))
+	withStyles(styles),
+	connect(mapStateToProps, mapDispatchToProps)
+)(withRouter(MenuBar));
--- src/components/Translations/MyProjectReportPopup.js
+++ src/components/Translations/MyProjectReportPopup.js
@@ -12,314 +12,276 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import apiUrl from "../GlobalUrl";
 import CircleLoader from "../loaders/CircleLoader";
+import {
+	bibleBookNewTestments,
+	bibleBookOldTestments,
+} from "../Common/BibleOldNewTestment";
 
 function LinearProgressWithLabel(props) {
-  return (
-    <Box display="flex" alignItems="center">
-      <Box>
-        <LinearProgress
-          variant="determinate"
-          value={props.completedValue}
-          style={{ width: "50px" }}
-        />
-      </Box>
-      <Box>
-        <Typography
-          variant="body1"
-          color="textSecondary"
-          style={{ fontSize: 9 }}
-        >{`${props.translatedValue} / ${props.value}`}</Typography>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box display="flex" alignItems="center">
+			<Box>
+				<LinearProgress
+					variant="determinate"
+					value={props.completedValue}
+					style={{ width: "50px" }}
+				/>
+			</Box>
+			<Box>
+				<Typography
+					variant="body1"
+					color="textSecondary"
+					style={{ fontSize: 9 }}
+				>{`${props.translatedValue} / ${props.value}`}</Typography>
+			</Box>
+		</Box>
+	);
 }
 
 const useStyles = makeStyles((theme) => createStyles({}));
 
 export default function SimplePopover(props) {
-  const classes = useStyles();
-  const [BookDetails, setBookDetails] = React.useState(null);
-  const [unTokenisedOldBooks, setUnTokenisedOldBooks] = React.useState(0);
-  const [unTokenisedNewBooks, setUnTokenisedNewBooks] = React.useState(0);
-  const [newTestmentBooks, setNewTestmentBooks] = React.useState(null);
-  const [oldTestmentBooks, setOldTestmentBooks] = React.useState(null);
-  const [open, setOpen] = React.useState(false);
-  const [scroll, setScroll] = React.useState("paper");
-  const [loading, setLoading] = React.useState(false);
-  const [proId, setProId] = React.useState(0);
+	const classes = useStyles();
+	const [BookDetails, setBookDetails] = React.useState(null);
+	const [unTokenisedOldBooks, setUnTokenisedOldBooks] = React.useState(0);
+	const [unTokenisedNewBooks, setUnTokenisedNewBooks] = React.useState(0);
+	const [newTestmentBooks, setNewTestmentBooks] = React.useState(null);
+	const [oldTestmentBooks, setOldTestmentBooks] = React.useState(null);
+	const [open, setOpen] = React.useState(false);
+	const [scroll, setScroll] = React.useState("paper");
+	const [loading, setLoading] = React.useState(false);
+	const [proId, setProId] = React.useState(0);
 
-  const handleClickOpen = (scrollType) => () => {
-    setLoading(true);
-    setOpen(true);
-    setScroll(scrollType);
-    const stoploading = () => {
-      setLoading(false);
-    };
-    if (proId != props.projectWiseId) {
-      fetch(
-        apiUrl + "/v1/autographamt/statistics/projects/" + props.projectWiseId
-      )
-        .then((results) => {
-          stoploading();
-          return results.json();
-        })
-        .then((data) => {
-          let matches = [];
-          if (
-            data.bookWiseData != null &&
-            Object.keys(data.bookWiseData).length != 0
-          ) {
-            const bibleBookOldTestments = [
-              "gen",
-              "exo",
-              "lev",
-              "num",
-              "deu",
-              "jos",
-              "jdg",
-              "rut",
-              "1sa",
-              "2sa",
-              "1ki",
-              "2ki",
-              "1ch",
-              "2ch",
-              "ezr",
-              "neh",
-              "est",
-              "job",
-              "psa",
-              "pro",
-              "ecc",
-              "sng",
-              "isa",
-              "jer",
-              "lam",
-              "ezk",
-              "dan",
-              "hos",
-              "jol",
-              "amo",
-              "oba",
-              "jon",
-              "mic",
-              "nam",
-              "hab",
-              "zep",
-              "hag",
-              "zec",
-              "mal",
-            ];
-            const bibleBookNewTestments = [
-              "mat",
-              "mrk",
-              "luk",
-              "jhn",
-              "act",
-              "rom",
-              "1co",
-              "2co",
-              "gal",
-              "eph",
-              "php",
-              "col",
-              "1th",
-              "2th",
-              "1ti",
-              "2ti",
-              "tit",
-              "phm",
-              "heb",
-              "jas",
-              "1pe",
-              "2pe",
-              "1jn",
-              "2jn",
-              "3jn",
-              "jud",
-              "rev",
-            ];
-            let oldTestments = [];
-            bibleBookOldTestments.map((book) => {
-              //map function to push old testament books in order
-              return props.projectBooks.includes(book)
-                ? oldTestments.push(book)
-                : null;
-            });
-            let newTestments = [];
-            bibleBookNewTestments.map((book) => {
-              //map function to push new testament books in order
-              return props.projectBooks.includes(book)
-                ? newTestments.push(book)
-                : null;
-            });
-            let oldBooks = [];
-            for (let book of oldTestments) {
-              //for order objects and also adding three code book name to the object
-              let booksKey = data.bookWiseData[book];
-              if (booksKey !== undefined) {
-                oldBooks.push(booksKey);
-              }
-            }
-            setUnTokenisedOldBooks(oldTestments.length - oldBooks.length);
-            setOldTestmentBooks(oldBooks);
-            let newBooks = [];
-            for (let book of newTestments) {
-              //for order objects and also adding three code book name to the object
-              let booksKey = data.bookWiseData[book];
-              if (booksKey !== undefined) {
-                newBooks.push(booksKey);
-              }
-            }
-            setUnTokenisedNewBooks(newTestments.length - newBooks.length);
-            setNewTestmentBooks(newBooks);
-          }
-          setBookDetails(matches);
-          setProId(props.projectWiseId);
-        });
-    } else {
-      stoploading();
-    }
-  };
+	const handleClickOpen = (scrollType) => () => {
+		setLoading(true);
+		setOpen(true);
+		setScroll(scrollType);
+		const stoploading = () => {
+			setLoading(false);
+		};
+		if (proId != props.projectWiseId) {
+			fetch(
+				apiUrl +
+					"/v1/autographamt/statistics/projects/" +
+					props.projectWiseId
+			)
+				.then((results) => {
+					stoploading();
+					return results.json();
+				})
+				.then((data) => {
+					let matches = [];
+					if (
+						data.bookWiseData != null &&
+						Object.keys(data.bookWiseData).length != 0
+					) {
+						let oldTestments = [];
+						bibleBookOldTestments.map((book) => {
+							//map function to push old testament books in order
+							return props.projectBooks.includes(book)
+								? oldTestments.push(book)
+								: null;
+						});
+						let newTestments = [];
+						bibleBookNewTestments.map((book) => {
+							//map function to push new testament books in order
+							return props.projectBooks.includes(book)
+								? newTestments.push(book)
+								: null;
+						});
+						let oldBooks = [];
+						for (let book of oldTestments) {
+							//for order objects and also adding three code book name to the object
+							let booksKey = data.bookWiseData[book];
+							if (booksKey !== undefined) {
+								oldBooks.push(booksKey);
+							}
+						}
+						setUnTokenisedOldBooks(
+							oldTestments.length - oldBooks.length
+						);
+						setOldTestmentBooks(oldBooks);
+						let newBooks = [];
+						for (let book of newTestments) {
+							//for order objects and also adding three code book name to the object
+							let booksKey = data.bookWiseData[book];
+							if (booksKey !== undefined) {
+								newBooks.push(booksKey);
+							}
+						}
+						setUnTokenisedNewBooks(
+							newTestments.length - newBooks.length
+						);
+						setNewTestmentBooks(newBooks);
+					}
+					setBookDetails(matches);
+					setProId(props.projectWiseId);
+				});
+		} else {
+			stoploading();
+		}
+	};
 
-  const handleCloses = () => {
-    setOpen(false);
-  };
+	const handleCloses = () => {
+		setOpen(false);
+	};
 
-  const descriptionElementRef = React.useRef(null);
-  React.useEffect(() => {
-    if (open) {
-      const { current: descriptionElement } = descriptionElementRef;
-      if (descriptionElement !== null) {
-        descriptionElement.focus();
-      }
-    }
-  }, [open]);
+	const descriptionElementRef = React.useRef(null);
+	React.useEffect(() => {
+		if (open) {
+			const { current: descriptionElement } = descriptionElementRef;
+			if (descriptionElement !== null) {
+				descriptionElement.focus();
+			}
+		}
+	}, [open]);
 
-  const columns = [
-    {
-      name: (
-        <span style={{ fontSize: "90%", fontWeight: "bold" }}>BOOK NAME</span>
-      ),
-      selector: "bookCode",
-      sortable: true,
-      cell: (row) => (
-        <React.Fragment>{`${row.bookName.toUpperCase()}`}</React.Fragment>
-      ),
-    },
-    {
-      name: (
-        <span style={{ fontSize: "90%", fontWeight: "bold" }}>
-          TOKEN TRANSLATION PROGRESS
-        </span>
-      ),
-      sortable: true,
-      cell: (row) => (
-        <div className={classes.fullWidth}>
-          <LinearProgressWithLabel
-            value={row.allTokensCount}
-            translatedValue={row.translatedTokensCount}
-            completedValue={row.completed}
-          />
-        </div>
-      ),
-    },
-    {
-      name: <span style={{ fontSize: "90%" }}>DRAFT PROGRESS</span>,
-      selector: "completed",
-      sortable: true,
-      cell: (row) => <React.Fragment>{`${row.completed}%`}</React.Fragment>,
-    },
-  ];
+	const columns = [
+		{
+			name: (
+				<span style={{ fontSize: "90%", fontWeight: "bold" }}>
+					BOOK NAME
+				</span>
+			),
+			selector: "bookCode",
+			sortable: true,
+			cell: (row) => (
+				<React.Fragment>{`${row.bookName.toUpperCase()}`}</React.Fragment>
+			),
+		},
+		{
+			name: (
+				<span style={{ fontSize: "90%", fontWeight: "bold" }}>
+					TOKEN TRANSLATION PROGRESS
+				</span>
+			),
+			sortable: true,
+			cell: (row) => (
+				<div className={classes.fullWidth}>
+					<LinearProgressWithLabel
+						value={row.allTokensCount}
+						translatedValue={row.translatedTokensCount}
+						completedValue={row.completed}
+					/>
+				</div>
+			),
+		},
+		{
+			name: <span style={{ fontSize: "90%" }}>DRAFT PROGRESS</span>,
+			selector: "completed",
+			sortable: true,
+			cell: (row) => (
+				<React.Fragment>{`${row.completed}%`}</React.Fragment>
+			),
+		},
+	];
 
-  return (
-    <div>
-      <Button
-        onClick={handleClickOpen("paper")}
-        size="small"
-        variant="contained"
-        style={{ fontSize: "80%", backgroundColor: "#21b6ae" }}
-      >
-        View
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleCloses}
-        scroll={scroll}
-        fullWidth={true}
-        maxWidth={"md"}
-        aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description"
-      >
-        <DialogTitle id="scroll-dialog-title">
-          <Grid container>
-            <Grid item sm={6} style={{ fontSize: "80%", fontWeight: "bold" }}>
-              Bookwise Translation Progress
-            </Grid>
-            <Grid
-              item
-              sm={6}
-              style={{
-                fontSize: "80%",
-                fontWeight: "bold",
-                textAlign: "right",
-              }}
-            >
-              {"Books Assigned  -  " + props.bookCount}
-            </Grid>
-          </Grid>
-        </DialogTitle>
+	return (
+		<div>
+			<Button
+				onClick={handleClickOpen("paper")}
+				size="small"
+				variant="contained"
+				style={{ fontSize: "80%", backgroundColor: "#21b6ae" }}
+			>
+				View
+			</Button>
+			<Dialog
+				open={open}
+				onClose={handleCloses}
+				scroll={scroll}
+				fullWidth={true}
+				maxWidth={"md"}
+				aria-labelledby="scroll-dialog-title"
+				aria-describedby="scroll-dialog-description"
+			>
+				<DialogTitle id="scroll-dialog-title">
+					<Grid container>
+						<Grid
+							item
+							sm={6}
+							style={{ fontSize: "80%", fontWeight: "bold" }}
+						>
+							Bookwise Translation Progress
+						</Grid>
+						<Grid
+							item
+							sm={6}
+							style={{
+								fontSize: "80%",
+								fontWeight: "bold",
+								textAlign: "right",
+							}}
+						>
+							{"Books Assigned  -  " + props.bookCount}
+						</Grid>
+					</Grid>
+				</DialogTitle>
 
-        <DialogContent dividers={scroll === "paper"}>
-          <div
-            id="scroll-dialog-description"
-            ref={descriptionElementRef}
-            tabIndex={-1}
-          >
-            {loading ? (
-              <CircleLoader />
-            ) : (
-              <div style={{ height: 400, width: "100%" }}>
-                {oldTestmentBooks != null && (
-                  <DataTable
-                    title={
-                      <span style={{ fontSize: "70%", fontWeight: "bold" }}>
-                        OLD TESTAMENT
-                      </span>
-                    }
-                    columns={columns}
-                    data={oldTestmentBooks}
-                  />
-                )}
-                &nbsp;&nbsp;&nbsp;
-                {unTokenisedOldBooks !== 0
-                  ? unTokenisedOldBooks + " " + "Untokenised Books"
-                  : "BOOKS TO BE TOKENISED"}
-                {newTestmentBooks != null && (
-                  <DataTable
-                    title={
-                      <span style={{ fontSize: "70%", fontWeight: "bold" }}>
-                        NEW TESTAMENT
-                      </span>
-                    }
-                    columns={columns}
-                    data={newTestmentBooks}
-                  />
-                )}
-                &nbsp;&nbsp;&nbsp;
-                {unTokenisedNewBooks !== 0
-                  ? unTokenisedNewBooks + " " + "Untokenised Books"
-                  : ""}
-              </div>
-            )}
-          </div>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloses} color="primary">
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+				<DialogContent dividers={scroll === "paper"}>
+					<div
+						id="scroll-dialog-description"
+						ref={descriptionElementRef}
+						tabIndex={-1}
+					>
+						{loading ? (
+							<CircleLoader />
+						) : (
+							<div style={{ height: 400, width: "100%" }}>
+								{oldTestmentBooks != null && (
+									<DataTable
+										title={
+											<span
+												style={{
+													fontSize: "70%",
+													fontWeight: "bold",
+												}}
+											>
+												OLD TESTAMENT
+											</span>
+										}
+										columns={columns}
+										data={oldTestmentBooks}
+									/>
+								)}
+								&nbsp;&nbsp;&nbsp;
+								{unTokenisedOldBooks !== 0
+									? unTokenisedOldBooks +
+									  " " +
+									  "Untokenised Books"
+									: "BOOKS TO BE TOKENISED"}
+								{newTestmentBooks != null && (
+									<DataTable
+										title={
+											<span
+												style={{
+													fontSize: "70%",
+													fontWeight: "bold",
+												}}
+											>
+												NEW TESTAMENT
+											</span>
+										}
+										columns={columns}
+										data={newTestmentBooks}
+									/>
+								)}
+								&nbsp;&nbsp;&nbsp;
+								{unTokenisedNewBooks !== 0
+									? unTokenisedNewBooks +
+									  " " +
+									  "Untokenised Books"
+									: ""}
+							</div>
+						)}
+					</div>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleCloses} color="primary">
+						Close
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</div>
+	);
 }
